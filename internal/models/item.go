7c4f77c0466e2a5ee32e8906c4b4430be597b7d0
Vyary <vyaryw@gmail.com>
2025-10-21 15:05:52 +0300

    update item struct

--- internal/models/item.go
+++ internal/models/item.go
@@ -1,29 +1,31 @@
 package models
 
+import "encoding/json"
+
 type Item struct {
-	ID              string `json:"market_id"`
-	Realm           string
-	W               int
-	H               int
-	Icon            string
-	Name            string `json:"name"`
-	BaseType        string `json:"base_type"`
-	Category        string `json:"category"`
-	SubCategory     string
-	Rarity          string
-	Support         bool
-	Desecrated      bool
-	Properties      string `json:"properties,omitempty"`
-	Requirements    string
-	EnchantMods     string
-	RuneMods        string
-	ImplicitMods    string
-	ExplicitMods    string
-	FracturedMods   string
-	DesecratedMods  string
-	FlavourText     string
-	DescrText       string
-	SecDescrText    string
-	IconTierText    string
-	GemSocketsCount int
+	ID              string          `json:"id"`
+	Realm           string          `json:"realm"`
+	W               int             `json:"w"`
+	H               int             `json:"h"`
+	Icon            string          `json:"icon"`
+	Name            string          `json:"name"`
+	BaseType        string          `json:"base_type"`
+	Category        string          `json:"category"`
+	SubCategory     string          `json:"sub_category"`
+	Rarity          string          `json:"rarity"`
+	Support         bool            `json:"support"`
+	Desecrated      bool            `json:"desecrated"`
+	Properties      json.RawMessage `json:"properties"`
+	Requirements    json.RawMessage `json:"requirements"`
+	EnchantMods     json.RawMessage `json:"enchantMods"`
+	RuneMods        json.RawMessage `json:"runeMods"`
+	ImplicitMods    json.RawMessage `json:"implicitMods"`
+	ExplicitMods    json.RawMessage `json:"explicitMods"`
+	FracturedMods   json.RawMessage `json:"fracturedMods"`
+	DesecratedMods  json.RawMessage `json:"desecratedMods"`
+	FlavourText     string          `json:"flavourText"`
+	DescrText       string          `json:"descrText"`
+	SecDescrText    string          `json:"secDescrText"`
+	IconTierText    string          `json:"iconTierText"`
+	GemSocketsCount int             `json:"gemSocketsCount"`
 }
